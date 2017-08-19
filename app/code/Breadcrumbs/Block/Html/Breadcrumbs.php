169e925a90de9aec7c7ecae63a78fb9d466b7a5c
Nguyen Cong Tuan Huy <tuanhuyhcmus@gmail.com>
2017-08-19 13:47:47 +0700

    custom breadcrumbs product-detail

---
+++ app/code/Breadcrumbs/Block/Html/Breadcrumbs.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace TMA\Breadcrumbs\Block\Html;
+
+
+class Breadcrumbs extends \Magento\Theme\Block\Html\Breadcrumbs {
+	
+}
---
+++ app/code/Breadcrumbs/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'TMA_Breadcrumbs',
+    __DIR__
+);
