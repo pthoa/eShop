31e41fd20f84a1038536203d51b668f933513900
Nguyen Cong Tuan Huy <tuanhuyhcmus@gmail.com>
2017-09-10 22:49:28 +0700

    update product-media in product-detail page

---
+++ app/code/Catalog/Block/Product/Compare/ListCompare.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace TMA\Catalog\Block\Product\Compare;
+
+use Magento\Catalog\Model\Product;
+use Magento\Customer\Model\Context;
+use Magento\Framework\App\Action\Action;
+
+/**
+ * Catalog products compare block
+ * @SuppressWarnings(PHPMD.CouplingBetweenObjects)
+ */
+class ListCompare extends \Magento\Catalog\Block\Product\Compare\ListCompare
+{
+	
+}
